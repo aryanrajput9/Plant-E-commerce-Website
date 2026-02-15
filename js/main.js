8463f83a9af53077685547d6f9f62c2f07113515
aryanrajput9 <babusahebaryan4@gmail.com>
2026-02-15 17:24:14 +0530

    update

--- js/main.js
+++ js/main.js
@@ -2,15 +2,12 @@ import { slideBar } from "./mobileNav.js";
 import { productTab } from "./producttab.js";
 import { getData } from "../data/Api.js";
 
-
 slideBar();
 
 const plants = await getData();
 
 if (!plants || plants.length === 0) {
     console.log("No plants received");
-    return;
+} else {
+    productTab(plants);
 }
-
-productTab(plants);
-
